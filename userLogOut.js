bdf635b8bb5ffca9cd4bfad36705c9f43905f0b1
Korbinian <kmr.ring@outlook.com>
2020-04-24 12:01:31 +0100

    added plan

--- userLogOut.js
+++ userLogOut.js
@@ -1,26 +1,20 @@
-const REQ = new XMLHttpRequest();
 
-let dataForLogOut =
-  '{"id": 0, "username": "MattyB", "password": "noice"}';
 
 function getLogOut() {
   REQ.open(
-    "GET",
-    "https://petstore.swagger.io/v2/user/logout?username=${}&password=${}"
+    "GET","http://petstore.swagger.io/v2/user/logout"
   );
   REQ.setRequestHeader("Content-Type", "Application/json");
   REQ.onload = () => {
     if (REQ.status === 200) {
       console.log(REQ.response);
-      console.log(REQ.response.title);
-      document.querySelector("#resp").innerHTML = REQ.response[0].title;
     } else {
       console.log(`Handle Error!`);
     }
   };
 
-  REQ.send(dataForLogOut);
+  REQ.send();
 }
 
-let buttGetLogout = document.querySelector("#butGetLogout");
-buttGetLogin.addEventListener("click", getLogOut);
+let buttGetLogout = document.querySelector("#buttGetLogout");
+buttGetLogout.addEventListener("click", getLogOut);
