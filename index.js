ca5a31ed4363e708ae6a1d179004055e87c3fe26
Korbinian <kmr.ring@outlook.com>
2020-04-24 09:58:48 +0100

    304

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const REQ = new XMLHttpRequest();
 
-let data = '{ "username" : "asdf" , "firstName" : "asdf2" , "lastName" : "asdf3" }';
+let data = '{"id": 0, "username" : "Kring","firstName": "Korbinian","lastName": "Ring","email": "k.ring@outlook.com","password": "root","phone": "23434134","userStatus": 0}'
 
 function createUser() {
     REQ.open('POST', 'http://petstore.swagger.io/v2/user');
     REQ.setRequestHeader('Content-Type', 'Application/json');
+    // REQ.setRequestHeader('Access-Control-Allow-Origin', '*');
+    // REQ.setRequestHeader('access-control-allow-methods', '*');
+    // REQ.setRequestHeader('access-control-allow-headers', '*');
     REQ.onload = () => {
         if (REQ.status === 201) {
             console.log(REQ.response);
@@ -12,8 +15,8 @@ function createUser() {
             console.log('handle error');
         }
     }
-    REQ.send(data);
+    REQ.send(data); // Waht we want to send across
 }
 
 let butt1 = document.querySelector('#butt1');
-butt1.addEventListener("click",createUser);
+butt1.addEventListener('click', createUser);
